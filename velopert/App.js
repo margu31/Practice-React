040f8e90b1f4775c732664bc590b4c7c4923e61b
margu31 <judypark31@gmail.com>
2021-03-18 14:06:12 +0900

    installed storybook

--- velopert/App.js
+++ velopert/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import MyComponent from './MyComponent';
+import Counter from './Counter';
+// import MyComponent from './MyComponent';
+
+// const App = () => {
+//   // return <MyComponent name="React" />;
+//   return <MyComponent>리액트</MyComponent>;
+// };
 
 const App = () => {
-  // return <MyComponent name="React" />;
-  return <MyComponent>리액트</MyComponent>;
+  return <Counter />;
 };
+
 
 export default App;
---
+++ velopert/Counter.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    // state의 초기값 설정
+    this.state = {
+      number: 0
+    };
+  }
+  
+  render() {
+    // state를 조회할 때는 this.state로 조회
+    const { number } = this.state; 
+    return (
+      <div>
+        <h1>{number}</h1>
+        <button
+          // onClick을 통해 버튼이 클릭되었을 때 호출할 함수를 지정
+          onClick={() => {
+            // this.setState를 사용하여 state에 새로운 값을 넣을 수 있다. 
+            this.setState({ number: number + 1 });
+          }}
+        >
+          +1
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
+
