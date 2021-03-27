c61d781297cc426ab3247bd630bca925f9f85b0a
margu31 <judypark31@gmail.com>
2021-03-27 14:36:11 +0900

    practice useReducer

--- online_velopert/App.js
+++ online_velopert/App.js
@@ -1,4 +1,5 @@
-import React, { useRef, useState, useMemo, useCallback } from 'react';
+// import React, { useRef, useState, useMemo, useCallback } from 'react';
+import React, { useRef, useReducer, useMemo, useCallback } from 'react';
 import UserList from './UserList';
 import CreateUser from './CreactUser';
 
@@ -15,23 +16,12 @@ function countActiveUsers(users) {
   return users.filter(user => user.active).length;
 }
 
-function App () {
-  const [inputs, setInputs] = useState({
+const initialState = {
+  inputs: {
     username: '',
     email: '',
-  });
-
-  const { username, email } = inputs;
-
-  const onChange = useCallback(e => {
-    const { name, value } = e.target;
-    setInputs({
-      ...inputs, 
-      [name]: value
-    });
-  }, [inputs]);
-
-  const [ users, setUsers ] = useState([
+  }, 
+  users: [
     {
       id: 1, 
       username: 'velopert',
@@ -50,50 +40,167 @@ function App () {
       email: 'muni1219@muniworld.com', 
       active: false,
     }
-  ]);
+  ]  
+}
 
+function reducer(state, action) {
+  switch (action.type) {
+    case 'CHANGE_INPUT':
+      return {
+        ...state, 
+        inputs: {
+          ...state.inputs,
+          [action.name]: action.value
+        }
+      };
+    case 'CREATE_USER':
+      return {
+        inputs: initialState.inputs,
+        users: state.users.concat(action.user)
+      }
+    case 'TOGGLE_USER':
+      return {
+        ...state, 
+        users: state.users.map(user => 
+          user.id === action.id 
+            ? {...user, active: !user.active}
+            : user
+          )
+      };
+    case 'REMOVE_USER':
+      return {
+        ...state, 
+        users: state.users.filter(user => user.id !== action.id)
+      }
+    default:
+      throw new Error('Unhandled action');
+  }
+}
+
+function App () {
+  // const [inputs, setInputs] = useState({
+  //   username: '',
+  //   email: '',
+  // });
+
+  // const { username, email } = inputs;
+
+  // const onChange = useCallback(e => {
+  //   const { name, value } = e.target;
+  //   setInputs({
+  //     ...inputs, 
+  //     [name]: value
+  //   });
+  // }, [inputs]);
+
+  // const [ users, setUsers ] = useState([
+  //   {
+  //     id: 1, 
+  //     username: 'velopert',
+  //     email:'public.velopert@gmail.com',
+  //     active: true,
+  //   },
+  //   {
+  //     id: 2, 
+  //     username: 'judy',
+  //     email: 'judydoll@judyworld.com',
+  //     active: false,
+  //   },
+  //   {
+  //     id: 3, 
+  //     username: 'muni',
+  //     email: 'muni1219@muniworld.com', 
+  //     active: false,
+  //   }
+  // ]);
+
+  // const nextId = useRef(4);
+
+  // const onCreate = useCallback(() => {
+  //   const user = {
+  //     id: nextId.current,
+  //     username, 
+  //     email,
+  //   };
+    
+  //   // setUsers([...users, user]);
+  //   setUsers(users => users.concat(user));
+  //   setInputs({
+  //     username: '',
+  //     email: ''
+  //   });
+  //   // console.log(nextId.current); // 4
+  //   nextId.current += 1;
+  // }, [username, email]);
+
+  // const onRemove = useCallback(id => {
+  //   setUsers(users => users.filter( user => user.id !== id  ))
+  // }, []);
+
+  // const onToggle = useCallback(id => {
+  //   setUsers(users => users.map(
+  //     user => user.id === id
+  //       ? { ...user, active: !user.active }
+  //       : user
+  //   ));
+  // }, []);
+
+  // const count = useMemo(() => countActiveUsers(users), [users]);
+
+  const [state, dispatch] = useReducer(reducer, initialState);
   const nextId = useRef(4);
+  const { users } = state;
+  const { username, email } = state.inputs; 
+
+  const onChange = useCallback(e => {
+    const { name, value } = e.target;
+    dispatch({
+      type: 'CHANGE_INPUT',
+      name,
+      value
+    })
+  }, []);
 
   const onCreate = useCallback(() => {
-    const user = {
-      id: nextId.current,
-      username, 
-      email,
-    };
-    
-    // setUsers([...users, user]);
-    setUsers(users => users.concat(user));
-    setInputs({
-      username: '',
-      email: ''
+    dispatch({
+      type: 'CREATE_USER',
+      user: {
+        id: nextId.current, 
+        username,
+        email,
+      }
     });
-    // console.log(nextId.current); // 4
     nextId.current += 1;
   }, [username, email]);
 
-  const onRemove = useCallback(id => {
-    setUsers(users => users.filter( user => user.id !== id  ))
-  }, []);
-
   const onToggle = useCallback(id => {
-    setUsers(users => users.map(
-      user => user.id === id
-        ? { ...user, active: !user.active }
-        : user
-    ));
+    dispatch({
+      type: 'TOGGLE_USER',
+      id
+    });
   }, []);
 
-  const count = useMemo(() => countActiveUsers(users), [users]);
+  const onRemove = useCallback(id => {
+    dispatch({
+      type: 'REMOVE_USER',
+      id
+    });
+  }, []);
+
+  const count = useMemo(() => countActiveUsers(users), [users])
 
   return (
     <>
-      <CreateUser 
+      {/* <CreateUser 
         username={username} 
         email={email}
         onChange={onChange}
         onCreate={onCreate}
-      />
-      <UserList users={users} onRemove={onRemove} onToggle={onToggle}/>
+      /> */}
+      {/* <UserList users={users} onRemove={onRemove} onToggle={onToggle}/> */}
+      {/* <div>활성 사용자 수: {count}</div> */}
+      <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate} />
+      <UserList users={users} onToggle={onToggle} onRemove={onRemove} />
       <div>활성 사용자 수: {count}</div>
     </>
   )
--- online_velopert/README.md
+++ online_velopert/README.md
@@ -1,6 +1,30 @@
 # PRACTICE REACT with Velopert Online Class
 
-
 - UseMemo : 연산된 값을 재사용
 - UseCallback : 특정 함수 재사용
 - React.memo : props가 바뀌었을 때에만 리렌더링, 렌더링된 결과를 재사용, 최적화
+
+- useReducer : action 객체를 기반으로 상태를 업데이트, 상태 업데이트 로직을 컴포넌트 밖으로 분리 가능
+
+  - action 객체 : 업데이트 할 때 참조하는 객체
+  - dispatch : action을 발생시키는 함수
+    - type: 어떤 업데이트를 할 건지 명시
+
+  ```jsx
+  function reducer(state, action) {
+    switch (action.type) {
+      case "INCREMENT":
+        return state + 1;
+      case "DECREMENT":
+        return state - 1;
+      default:
+        return state;
+    }
+  }
+  ```
+
+  ```jsx
+  const [number, dispatch] = useReducer(reducer, 0);
+  ```
+
+- 커스텀 Hook
